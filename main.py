e363dab863f17aead90e9d69ea6521fc0679bb84
ALVARIX18 <51389148+ALVARIX18@users.noreply.github.com>
2025-09-23 00:27:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ VOL_MULT_STRONG = float(os.environ.get("VOL_MULT_STRONG", 1.8))
 VOL_MULT_WEAK = float(os.environ.get("VOL_MULT_WEAK", 1.2))
 PRICE_ACCEL_THRESHOLD = float(os.environ.get("PRICE_ACCEL_THRESHOLD", 0.007))
 SCORE_ALERT = int(os.environ.get("SCORE_ALERT", 40))
-SCORE_SIGNAL = int(os.environ.get("SCORE_SIGNAL", 65))
+SCORE_SIGNAL = int(os.environ.get("SCORE_SIGNAL", 40))
 
 MAX_ALERTS_PER_SYMBOL_PER_DAY = int(os.environ.get("MAX_ALERTS_PER_SYMBOL_PER_DAY", 1))
 MAX_TRADES_PER_DAY = int(os.environ.get("MAX_TRADES_PER_DAY", 7))
